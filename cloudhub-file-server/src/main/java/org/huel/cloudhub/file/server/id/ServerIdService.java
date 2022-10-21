f3d2ffcdbf2699336a49cb35fb6b4de23c6d61aa
RollW <73376341+Roll-W@users.noreply.github.com>
2022-10-21 22:24:42 +0800

    feat: add ID service.

---
+++ cloudhub-file-server/src/main/java/org/huel/cloudhub/file/server/id/ServerIdService.java
@@ -0,0 +1,50 @@
+package org.huel.cloudhub.file.server.id;
+
+import com.google.common.io.Files;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
+
+/**
+ * @author RollW
+ */
+@Service
+public class ServerIdService {
+    private final UUID uuid;
+    private final String uuidString;
+
+    public ServerIdService() throws IOException {
+        uuid = initialId();
+        uuidString = uuid.toString();
+    }
+
+    private UUID initialId() throws IOException {
+        File file = new File("ID_VERSION");
+        if (!file.exists()) {
+            file.createNewFile();
+            UUID uid = UUID.randomUUID();
+            Files.write(uid.toString().getBytes(StandardCharsets.UTF_8), file);
+            return uid;
+        }
+        String firstLine = Files.asCharSource(file, StandardCharsets.UTF_8).readFirstLine();
+        if (firstLine == null) {
+            boolean delete = file.delete();
+            if (!delete) {
+                throw new IllegalStateException("failed delete ID_VERSION file.");
+            }
+            return initialId();
+        }
+        return UUID.fromString(firstLine);
+    }
+
+    public String getServerId() {
+        return uuidString;
+    }
+
+    public UUID getServerUuid() {
+        return uuid;
+    }
+}
