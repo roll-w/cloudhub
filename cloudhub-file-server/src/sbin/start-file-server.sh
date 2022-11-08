24d74e951e1b2260c74a30ebfdcc7e101ae23349
RollW <73376341+Roll-W@users.noreply.github.com>
2022-11-08 22:27:08 +0800

    feat: add file-server start shell script.

---
+++ cloudhub-file-server/src/sbin/start-file-server.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+export JAVA_HOME=$JAVA_HOME
+
+DIRNAME=$0
+if [ "${DIRNAME:0:1}" = "/" ];then
+    CURDIR=$(dirname "$DIRNAME")
+else
+    CURDIR="$(pwd)"/"$(dirname "$DIRNAME")"
+fi
+CLOUDHUB_FILE_HOME=$CURDIR/../
+
+if [ "$CLOUDHUB_FILE_HOME" = '' ]; then
+  echo "Not set CLOUDHUB_FILE_HOME variable in the env, program exit with code 2."
+  exit 2
+fi
+
+CONF_DIR=$CLOUDHUB_FILE_HOME/conf
+
+cd "$CLOUDHUB_FILE_HOME" || echo "Not found the path $CLOUDHUB_FILE_HOME, program exit with code 3." exit 3
+
+echo "Conf dir is in the $CONF_DIR. If you need changes settings, please follow direction to modify files in the dir."
+echo "Starting cloudhub-file-server......"
+
+exec "$JAVA_HOME"/bin/java -jar bin/cloudhub-file-server.jar --conf "$CONF_DIR"
