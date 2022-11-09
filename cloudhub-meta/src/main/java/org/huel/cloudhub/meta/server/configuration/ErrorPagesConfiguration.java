ba6388a7720807507b2a16f5b3e5ce3896a320c0
RollW <73376341+Roll-W@users.noreply.github.com>
2022-11-09 19:39:17 +0800

    feat: add error pages.

---
+++ cloudhub-meta/src/main/java/org/huel/cloudhub/meta/server/configuration/ErrorPagesConfiguration.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2022 Lingu.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.huel.cloudhub.meta.server.configuration;
+
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.ErrorPageRegistrar;
+import org.springframework.boot.web.server.ErrorPageRegistry;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+
+/**
+ * @author RollW
+ */
+@Configuration
+public class ErrorPagesConfiguration implements ErrorPageRegistrar {
+    @Override
+    public void registerErrorPages(ErrorPageRegistry registry) {
+        registry.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/404.html"));
+        registry.addErrorPages(new ErrorPage("/error.html"));
+    }
+}
