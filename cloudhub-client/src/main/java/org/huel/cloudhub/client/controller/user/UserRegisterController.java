e2953707d3202108d2c3f83ad3b069ed13fc8a65
RollW <73376341+Roll-W@users.noreply.github.com>
2022-11-15 11:50:00 +0800

    style: code style.

--- cloudhub-client/src/main/java/org/huel/cloudhub/client/controller/user/UserRegisterController.java
+++ cloudhub-client/src/main/java/org/huel/cloudhub/client/controller/user/UserRegisterController.java
@@ -1,12 +1,11 @@
 package org.huel.cloudhub.client.controller.user;
 
 import org.apache.commons.lang3.Validate;
-import org.huel.cloudhub.common.HttpResponseEntity;
-import org.huel.cloudhub.common.MessagePackage;
 import org.huel.cloudhub.client.data.dto.UserInfo;
 import org.huel.cloudhub.client.data.dto.request.UserRegisterRequest;
 import org.huel.cloudhub.client.service.user.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.huel.cloudhub.common.HttpResponseEntity;
+import org.huel.cloudhub.common.MessagePackage;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +17,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserRegisterController {
     static final String MISSING_PARAM = "Param %s cannot be empty.";
+    private final UserService userService;
+
+    public UserRegisterController(UserService userService) {
+        this.userService = userService;
+    }
 
     @PostMapping("/register")
     public HttpResponseEntity<UserInfo> register(@RequestBody UserRegisterRequest registerRequest) {
@@ -36,9 +40,4 @@ public class UserRegisterController {
         );
     }
 
-    UserService userService;
-    @Autowired
-    private void setUserService(UserService userService) {
-        this.userService = userService;
-    }
 }
