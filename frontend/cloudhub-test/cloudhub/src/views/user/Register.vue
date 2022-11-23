4db7099f353db8fe15ea18cf20e01a161eef61d0
杨程博 <719083363@qq.com>
2022-11-23 05:12:49 +0000

    update frontend/cloudhub-test/cloudhub/src/views/user/Register.vue.
    Signed-off-by: 杨程博 <719083363@qq.com>

--- frontend/cloudhub-test/cloudhub/src/views/user/Register.vue
+++ frontend/cloudhub-test/cloudhub/src/views/user/Register.vue
@@ -1,37 +1,165 @@
 <template>
-  <div class="row justify-content-md-center">
-    <div class="col-3">
+  <div class="container">
+    <div class="form-con">
+      <img :src="Img" alt="">
+      <p class="p-dark">Register</p>
+      <br/>
       <form>
-        <div class="mb-3">
-          <label for="username" class="form-label">用户名</label>
-          <input  type="text" class="form-control" id="username">
+
+        <div class="input-con">
+          <i class="fas fa-user icon"></i>
+          <input v-model="user.username" type="text" placeholder="Username">
         </div>
-        <div class="mb-3">
-          <label for="password" class="form-label">密码</label>
-          <input  type="password" class="form-control" id="password">
+
+        <div class="input-con">
+          <i class="fas fa-lock icon"></i>
+          <input v-model="user.password" type="password" placeholder="Password">
         </div>
-        <div class="mb-3">
-          <label for="password_confirm" class="form-label">确认密码</label>
-          <input  type="password" class="form-control" id="password_confirm">
+
+        <button type="button" @click="register"><p>Register</p></button>
+
+        <div>
+          Have an account?
+          <router-link :to="{name:'login_index'}" class="link-primary" style="text-decoration: none">Log in
+          </router-link>
         </div>
-        <button type="submit" class="btn btn-primary">注册</button>
       </form>
+
+
     </div>
+
   </div>
 </template>
 
 <script>
-import ContentBase from "@/components/ContentBase";
+
+import Img from "@/assets/images/register.jpg"
+
 export default {
-  // eslint-disable-next-line vue/multi-word-component-names
-  name: "Register",
-  components:{
-    // eslint-disable-next-line vue/no-unused-components
-    ContentBase
+  name: "RegisterView",
+
+  setup() {
+
+
+    const user = {
+      username: "admin",
+      password: "admin"
+    }
+
+    const register = () => {
+    }
+
+    return {
+      Img,
+      user,
+      register
+    }
   }
 }
+
 </script>
 
 <style scoped>
 
+/*容器div*/
+.container {
+  font-family: 'Roboto', sans-serif;
+  height: 100%;
+  width: 100%;
+  position: relative;
+  top: 0;
+  left: 0;
+
+}
+
+/*内容div*/
+.form-con {
+  width: 400px;
+  height: 550px;
+  margin: 50px auto;
+  background-color: #ffffff;
+  border-radius: 25px;
+  overflow: hidden;
+}
+
+.form-con form {
+  display: flex;
+  flex-wrap: wrap;
+  padding: 0 0 100px 0;
+}
+
+.form-con img {
+  display: block;
+  width: 80%;
+  margin-top: -50px;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.form-con .p-dark, .p-light {
+  font-size: 16px;
+  text-align: center;
+  height: 30px;
+}
+
+.form-con .p-dark {
+  font-weight: 700;
+  color: #515151;
+  font-size: 24px;
+  margin: -20px 0 0 0;
+}
+
+.form-con .p-light {
+  font-weight: 100;
+  color: #c5c5c5;
+  margin: 0 0 30px 0;
+}
+
+.form-con form .input-con {
+  width: 80%;
+  margin: auto;
+}
+
+.form-con form .input-con .icon {
+  position: relative;
+  top: 32px;
+  left: 20px;
+  color: #ff4f5a;
+}
+
+.form-con form .input-con input {
+  width: 100%;
+  height: 45px;
+  margin: 0px auto;
+  border-radius: 25px;
+  outline: none;
+  border: 2px solid #eeeeee;
+  font-size: 18px;
+  padding: 0 40px;
+  box-sizing: border-box;
+}
+
+.form-con form button {
+  background-color: #1b2e35;
+  border: 0;
+  width: 80%;
+  height: 50px;
+  margin: 25px auto;
+  border-radius: 25px;
+  outline: none;
+  cursor: pointer;
+}
+
+.form-con form button p {
+  color: #ffffff;
+  font-size: 16px;
+  font-weight: 700;
+}
+
+.form-con form input::placeholder {
+  color: #c7c7c7;
+  font-weight: 100;
+  font-size: 16px;
+  font-family: 'Roboto', sans-serif;
+}
 </style>
