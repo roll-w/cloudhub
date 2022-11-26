0a014acb618320212ad7780bbdd8aa6beadddb3e
RollW <73376341+Roll-W@users.noreply.github.com>
2022-11-26 15:54:06 +0800

    feat: collapsable sidebar.

--- frontend/cloudhub-test/cloudhub/src/components/common/NavBar.vue
+++ frontend/cloudhub-test/cloudhub/src/components/common/NavBar.vue
@@ -1,52 +1,56 @@
 <template>
-  <nav class="d-flex flex-column flex-shrink-0 p-4 bg-light border-end border-1" data-bs-scroll="true"
-       data-bs-backdrop="false" tabindex="-1">
-
-    <h2>
-      <router-link class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none"
-                   :to="{name:'cloudhub_index'}">CloudHub
-      </router-link>
-    </h2>
-    <hr>
-    <ul class="nav nav-pills flex-column mb-auto">
-      <li>
-        <router-link :class="route_name === 'cloudhub_index' ? 'navbar-active' :'nav-link link-dark' "
-                     :to="{name:'cloudhub_index'}">总览
+  <button class="navbar-toggler float-start bg-secondary" type="button" data-bs-toggle="collapse" data-bs-target="#navSidebar"
+          aria-expanded="false" aria-controls="navSidebar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse collapse-horizontal show" id="navSidebar">
+    <nav class="d-flex flex-grow-0 flex-column flex-shrink-0 p-4 bg-light border-end border-1 h-100" data-bs-scroll="true"
+         data-bs-backdrop="false" tabindex="-1">
+      <h2>
+        <router-link class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none"
+                     :to="{name:'cloudhub_index'}">CloudHub
         </router-link>
-      </li>
-      <li>
-        <router-link :class="route_name === 'bucket_index' ? 'nav-link active' :'nav-link link-dark' "
-                     :to="{name:'bucket_index'}">桶管理
-        </router-link>
-      </li>
-      <li>
-        <router-link :class="route_name === 'userList_index' ? 'nav-link active' :'nav-link link-dark' "
-                     :to="{name:'userList_index'}">用户管理
-        </router-link>
-      </li>
-
-      <li>
-        <router-link :class="route_name === 'metadataserver_index' ? 'nav-link active' :'nav-link link-dark' "
-                     :to="{name:'metadataserver_index'}">文件集群信息
-        </router-link>
-      </li>
-    </ul>
-    <hr>
-    <div class="dropdown">
-      <a href="#" class="d-flex align-items-center link-dark text-decoration-none dropdown-toggle"
-         id="dropdownUser2" data-bs-toggle="dropdown" aria-expanded="false">
-        <strong>Hello </strong>
-      </a>
-      <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownUser2">
-        <li><a class="dropdown-item">Settings</a></li>
+      </h2>
+      <hr>
+      <ul class="nav nav-pills flex-column mb-auto">
         <li>
-          <hr class="dropdown-divider">
+          <router-link :class="route_name === 'cloudhub_index' ? 'navbar-active' :'nav-link link-dark' "
+                       :to="{name:'cloudhub_index'}">总览
+          </router-link>
         </li>
-        <li><a class="dropdown-item">Logout</a></li>
-      </ul>
-    </div>
-  </nav>
+        <li>
+          <router-link :class="route_name === 'bucket_index' ? 'nav-link active' :'nav-link link-dark' "
+                       :to="{name:'bucket_index'}">桶管理
+          </router-link>
+        </li>
+        <li>
+          <router-link :class="route_name === 'userList_index' ? 'nav-link active' :'nav-link link-dark' "
+                       :to="{name:'userList_index'}">用户管理
+          </router-link>
+        </li>
 
+        <li>
+          <router-link :class="route_name === 'metadataserver_index' ? 'nav-link active' :'nav-link link-dark' "
+                       :to="{name:'metadataserver_index'}">文件集群信息
+          </router-link>
+        </li>
+      </ul>
+      <hr>
+      <div class="dropdown">
+        <a href="#" class="d-flex align-items-center link-dark text-decoration-none dropdown-toggle"
+           id="dropdownUser2" data-bs-toggle="dropdown" aria-expanded="false">
+          <strong>{{ $store.state.user.username }}</strong>
+        </a>
+        <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownUser2">
+          <li><a class="dropdown-item">Settings</a></li>
+          <li>
+            <hr class="dropdown-divider">
+          </li>
+          <li><a class="dropdown-item">Logout</a></li>
+        </ul>
+      </div>
+    </nav>
+  </div>
 </template>
 
 <script>
