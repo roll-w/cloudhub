12abea9cfbda4cd4b7b1fb1c4abcdbf9468b7389
ycb <719083363@qq.com>
2022-11-27 16:28:45 +0800

    修复实时折线图显示问题;优化代码结构

--- frontend/cloudhub-test/cloudhub/src/components/echarts/storage/StorageIO.vue
+++ frontend/cloudhub-test/cloudhub/src/components/echarts/storage/StorageIO.vue
@@ -9,7 +9,7 @@
 import {ref, onMounted, getCurrentInstance} from 'vue'
 
 export default {
-  name: "ReceiveAndSend",
+  name: "StorageIO",
   setup() {
     const {proxy} = getCurrentInstance() // 获取全局配置项
     const myRef = ref(null) // 获取dom实例
@@ -22,27 +22,22 @@ export default {
       // 基于准备好的dom，初始化echarts实例
       const myChart = proxy.$echarts.init(myRef.value)
 
-      // 变量
-      let base = +new Date(0, 0, 0);
-      let oneDay = 24 * 3600 * 1000;
-      let date = [];
-      let receiveRate = [Math.random() * 150];
-      let now = new Date(base);
 
-      // 方法等
-      function addData(shift) {
-        now = [now.getFullYear(), now.getMonth() + 1, now.getDate()].join('/');
-        date.push(now);
-        receiveRate.push((Math.random() - 0.4) * 10 + receiveRate[receiveRate.length - 1]);
-        if (shift) {
-          date.shift();
-          receiveRate.shift();
-        }
-        now = new Date(+new Date(now) + oneDay);
-      }
+      // 一次展示的数据的条数
+      let arrLen = 10;
 
-      for (let i = 1; i < 10; i++) {
-        addData();
+      // 内存的写入速率;磁盘的写入速率;磁盘的读速率
+      let memWrite = [];
+      let diskRead = [];
+      let diskWrite = [];
+      let x = []; // x 轴
+
+      // 一次展示 10 条数据
+      for (let i = 0; i < arrLen; i++){
+        memWrite.push(0);
+        diskWrite.push(0);
+        diskRead.push(0);
+        x.push(0);
       }
 
       // 指定图表的配置项和数据
@@ -50,16 +45,9 @@ export default {
         backgroundColor: '#fff',
         tooltip: {
           trigger: 'axis',
-          formatter: function (params) {
-            let res = ''
-            res += '<div>' + params[0].seriesName + ':' + params[0].data + '</div><br>'
-            res += '<div>' + params[1].seriesName + ':' + params[1].data + '</div>'
-            return res;
-          },
-
         },
         title: {
-          text: `{a|存储器IO速率}`,
+          text: `{a|存储器IO速率(bytes/s)}`,
           textStyle: {
             rich: {
               a: {
@@ -72,7 +60,7 @@ export default {
           left: '2%',
         },
         legend: {
-          // data: ['发送速率(bytes/s)', '接收速率(bytes/s)'],
+          data: ['内存写入','磁盘读入', '磁盘写出'],
           textStyle: {
             align: 'right',
           },
@@ -85,173 +73,114 @@ export default {
           bottom: '3%',
           containLabel: true,
         },
+        // 设置x轴的样式
         xAxis: {
           type: 'category',
-          boundaryGap: false,
           show: false,
-          axisLabel: {
-            //坐标轴刻度标签的相关设置。
-            interval: 0, //设置为 1，表示『隔一个标签显示一个标签』
-            //	margin:15,
-            textStyle: {
-              color: '#1B253A',
-              fontStyle: 'normal',
-              fontFamily: '微软雅黑',
-              fontSize: 12,
+          boundaryGap: false, //坐标轴两边留白
+        },
+        // 设置y轴的样式
+        yAxis: [
+          {
+            type: 'value',
+            axisLabel: {
+              textStyle: {
+                color: '#a8aab0',
+                fontStyle: 'normal',
+                fontFamily: '微软雅黑',
+                fontSize: 12,
+              },
+              // formatter: '{value}' // 设置y轴数据样式
+            },
+            axisLine: {
+              show: false,
             },
             axisTick: {
               show: false,
             },
-          },
-          axisLine: {
-            lineStyle: {
-              color: '#E5E9ED',
-              // opacity:0.2
-            },
-          },
-          splitLine: {
-            show: false,
-          },
-        },
-        yAxis: {
-          boundaryGap: [0, '50%'],
-          type: 'value',
-          axisLabel1: {
-            textStyle: {
-              color: '#a8aab0',
-              fontStyle: 'normal',
-              fontFamily: '微软雅黑',
-              fontSize: 12,
-            },
-          },
-          axisLine: {
-            show: false,
-          },
-          axisTick: {
-            show: false,
-          },
-          splitLine: {
-            show: true,
-            lineStyle: {
-              color: '#E5E9ED',
-              // 	opacity:0.1
-            },
-          },
-          axisLabel2: {
-            show: true,
-            margin: 20,
-            formatter: '{value}',
-          },
-        },
-        series: [
-          {
-            name: '内存写(bytes/s)',
-            type: 'line',
-            smooth: true,
-            stack: 'Total',
-            itemStyle: { // 折线颜色(浅绿)
-              normal: {
-                color: '#ABEBC6',
-                lineStyle: {
-                  color: '#1EE882',
-                  width: 1,
-                },
-                areaStyle: {
-                  // color: new echarts.graphic.LinearGradient(0, 1, 0, 0, [
-                  //   {
-                  //     offset: 0,
-                  //     color: 'rgba(30,232,130,0)',
-                  //   },
-                  //   {
-                  //     offset: 1,
-                  //     color: 'rgba(30,232,130,0.5)',
-                  //   },
-                  // ]),
-
-                },
+            splitLine: {
+              show: true,
+              lineStyle: {
+                color: '#E5E9ED',
+                // 	opacity:0.1
               },
             },
-            emphasis: {
-              focus: 'series'
-            },
-            data: receiveRate
           },
-          {
-            name: '磁盘写(bytes/s)', // 浅黄
-            type: 'line',
-            smooth: true,
-            stack: 'Total',
-            itemStyle: {
-              normal: {
-                color: '#F9E79F', // 区域颜色
-                lineStyle: {
-                  color: 'rgba(253,222,9,1)',
-                  width: 1,
-                },
-                areaStyle: {
-                  // color: new echarts.graphic.LinearGradient(0, 1, 0, 0, [
-                  //   {
-                  //     offset: 0,
-                  //     color: 'rgba(253,222,9,0)',
-                  //   },
-                  //   {
-                  //     offset: 1,
-                  //     color: 'rgba(253,222,9,0.5)',
-                  //   },
-                  // ]),
-                },
-              },
-            },
-            emphasis: {
-              focus: 'series'
-            },
-            data: receiveRate
-          },
-          {
-            name: '磁盘读(bytes/s)', // 浅黄
-            type: 'line',
-            smooth: true,
-            stack: 'Total',
-            itemStyle: {
-              normal: {
-                color: '#AED6F1 ', // 区域颜色
-                lineStyle: {
-                  color: '#3498DB',
-                  width: 1,
-                },
-                areaStyle: {
-                  // color: new echarts.graphic.LinearGradient(0, 1, 0, 0, [
-                  //   {
-                  //     offset: 0,
-                  //     color: 'rgba(253,222,9,0)',
-                  //   },
-                  //   {
-                  //     offset: 1,
-                  //     color: 'rgba(253,222,9,0.5)',
-                  //   },
-                  // ]),
-                },
-              },
-            },
-            emphasis: {
-              focus: 'series'
-            },
-            data: receiveRate
-          }
-        ]
+        ],
       })
       setInterval(function () {
+        // 删除数组的首个元素,再数组尾部添加新的元素(实现从右向左动态更新的视觉效果)
+        function addData(shift) {
+          x.push(0);
+
+          memWrite.push(Math.random().toFixed(2))      // ####### 从接口获取数据
+          diskRead.push(Math.random().toFixed(2));        // ######## 从接口获取数据
+          diskWrite.push(Math.random().toFixed(2));     // ######## 从接口获取数据
+
+          if (shift) {
+            x.shift();  // 移除数组的首个元素
+            memWrite.shift()
+            diskRead.shift();
+            diskWrite.shift();
+          }
+        }
         addData(true);
         myChart.setOption({
-          xAxis: {
-            // receiveRate: date
-          },
-          series: [{
-            name: '',
-            data: receiveRate
-          }]
+          series: [
+            {
+              name: '内存写入',
+              type: 'line',
+              itemStyle: {
+                normal: {
+                  color: 'rgba(253,222,9,1)',
+                  lineStyle: {
+                    color: 'rgba(253,222,9,1)',
+                    width: 1,
+                  },
+                  areaStyle: {
+                    color: '#F9E79F'
+                  },
+                },
+              },
+              data: memWrite,
+            },
+            {
+              name: '磁盘读出',
+              type: 'line',
+              itemStyle: {
+                normal: {
+                  color: '#5DADE2',
+                  lineStyle: {
+                    color: '#5DADE2',
+                    width: 1,
+                  },
+                  areaStyle: {
+                    color: '#AED6F1'
+                  },
+                },
+              },
+              data: diskRead,
+            },
+            {
+              name: '磁盘写入',
+              type: 'line',
+              itemStyle: {
+                normal: {
+                  color: '#1EE882',
+                  lineStyle: {
+                    color: '#1EE882',
+                    width: 1,
+                  },
+                  areaStyle: {
+                    color: '#ABEBC6'
+                  },
+                },
+              },
+              data: diskWrite,
+            },
+          ]
         });
-      }, 1000);
+      }, 1000); // 定时器
     }
 
     return {
