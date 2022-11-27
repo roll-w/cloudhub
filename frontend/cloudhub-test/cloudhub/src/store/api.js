673b018b65873e066e39ea1092c5842e42ef0a17
狗崽子 <1945553613@qq.com>
2022-11-27 12:13:47 +0800

    修改删除用户bug

--- frontend/cloudhub-test/cloudhub/src/store/api.js
+++ frontend/cloudhub-test/cloudhub/src/store/api.js
@@ -1,5 +1,5 @@
 //import url from '@/store/api' 建议使用此方法调用url
-const server = "http://localhost:7010/";
+const server = "http://localhost:7010";
 
 // 认证
 const url_login = server +"api/user/login";
@@ -8,14 +8,14 @@ const url_getCurrent = server + "api/user/current"
 const url_logout = server + "api/user/logout";
 
 //桶管理
-const url_addBucket = server + "api/bucket/create";
-const url_getBucket =server + "api/bucket/get/all";
-const url_deleteBucket =server + "api/bucket/delete";
-const url_settingVisibility = server + "api/bucket/setting/visibility";
+const url_addBucket = server + "/api/bucket/create";
+const url_getBucket =server + "/api/bucket/get/all";
+const url_deleteBucket =server + "/api/bucket/delete";
+const url_settingVisibility = server + "/api/bucket/setting/visibility";
 
 //管理员:  用户管理
-const url_getUser = server + "api/admin/user/get/all"
-const url_deleteUserById = server + "api/admin/user/delete"
+const url_getUser = server + "/api/admin/user/get/all"
+const url_deleteUserById = server + "/api/admin/user/delete"
 const url_getUserByName = server + "/api/admin/user/get";
 
 export default {
--- frontend/cloudhub-test/cloudhub/src/views/HomeView.vue
+++ frontend/cloudhub-test/cloudhub/src/views/HomeView.vue
@@ -46,10 +46,5 @@ main{
 .allow-rolling{
   overflow: auto;
 }
-
-/* 由于作为根组件的APP中的元素 position有值 fixed,因此为使得对话框正常显示需要添加此CSS语句  */
-/*.modal-backdrop {*/
-/*  z-index: -1;*/
-/*}*/
 </style>
 
