50e2de85f27360ef9de0771e99cc45184434ed8c
狗崽子 <1945553613@qq.com>
2022-11-27 17:51:40 +0800

    添加radio

--- frontend/cloudhub-test/cloudhub/src/views/bucket/BucketView.vue
+++ frontend/cloudhub-test/cloudhub/src/views/bucket/BucketView.vue
@@ -9,7 +9,7 @@
       <div class="d-flex flex-fill justify-content-end">
         <form @submit.prevent="getBucketByName">
           <div class="input-group">
-            <input v-model="bucketName" type="text" class="form-control"
+            <input v-model="btName" type="text" class="form-control"
                    placeholder="搜索桶......">
             <button class="btn btn-outline-primary" type="submit">查询</button>
           </div>
@@ -66,14 +66,14 @@
                 <label for="name" class="form-label">名称:</label>
                 <input type="text" v-model="bucketName" class="form-control" id="name" placeholder="Name">
               </div>
-              <div class="mb-3">
-                <label for="name" class="form-label">策略:</label>
-                <input type="text" v-model="visibility" class="form-control" id="name" placeholder="Visibility">
+              <div class="pb-3 form-floating">
+                <select class="form-select" id="bucket-vis-select" @click="getValue">
+                  <option value="PRIVATE">私有读写</option>
+                  <option value="PUBLIC_READ">公共读</option>
+                  <option value="PUBLIC_READ_WRITE">公共读写</option>
+                </select>
+                <label for="name" class="form-label">设置新的桶策略：</label>
               </div>
-              <figure class="text-center">
-                <!--                后期修改样式-->
-                PRIVATE or PUBLIC_READ or PUBLIC_READ_WRITE
-              </figure>
               <div class="modal-footer">
                 <button style="margin-right: 5px" type="button" class="btn btn-secondary" data-bs-dismiss="modal">取消
                 </button>
@@ -84,6 +84,7 @@
         </div>
       </div>
     </div>
+
     <div class="modal fade" ref="bucketVisibilityModel" id="bucketAuthority"
          tabindex="-1" aria-hidden="true">
       <div class="modal-dialog">
@@ -95,9 +96,9 @@
           <div class="modal-body">
 
             <form @submit.prevent="settingVisibility">
-              <input ref="visBucketName" type="text" v-model="bucketName" hidden id="name" placeholder="Name">
+              <input ref="visBucketName" type="text" v-model="name" hidden id="name" placeholder="Name">
               <div class="pb-3 form-floating">
-                <select class="form-select" id="bucket-vis-select">
+                <select class="form-select" id="bucket-vis-select" @click="getValue">
                   <option value="PRIVATE">私有读写</option>
                   <option value="PUBLIC_READ">公共读</option>
                   <option value="PUBLIC_READ_WRITE">公共读写</option>
@@ -134,20 +135,31 @@ export default {
   },
 
   setup() {
-    //接口
     let buckets = ref([]);
-    let bucketName = ref([]);
-    let visibility = ref([]);
-    const router = useRouter()
-    const bucketVisibilityModel = ref(null)
+    let bucketName = ref('');
+    let visibility = ref('');
+    let name = ref('');
+    let val = ref('');
+    let btName =ref('');
+    const router = useRouter();
+    const bucketVisibilityModel = ref(null);
 
     onMounted(() => {
+
       bucketVisibilityModel.value.addEventListener("show.bs.modal", (event) => {
         const button = event.relatedTarget
         let bucketName = button.getAttribute("data-cfs-bucket-name")
-        console.log("We got bucket name=" + bucketName)
+        name.value = bucketName
       })
     })
+
+    const getValue = (event)=> {
+      let radioVal = event.target.value;
+      visibility.value =radioVal;
+      val.value = radioVal;
+    }
+
+
     const checkFile = () => {
       router.push('file')
       //获取指定桶的文件信息
@@ -173,7 +185,6 @@ export default {
         },
         crossDomain: true,
         success(resp) {
-          console.log(resp.data)
           buckets.value = resp.data;
           console.log("Successfully obtained the bucket list！")
         },
@@ -245,8 +256,8 @@ export default {
         crossDomain: true,
         contentType: "application/json;charset=UTF-8",
         data: JSON.stringify({
-          bucketName: bucketName.value,
-          visibility: visibility.value,
+          bucketName: name.value,
+          visibility: val.value,
         }),
         success(resp) {
           if (resp.errorCode === "00000") {
@@ -269,7 +280,7 @@ export default {
         crossDomain: true,
         type: "GET",
         data: {
-          bucketName: bucketName.value,
+          bucketName: btName.value,
         },
         success(resp) {
           if (resp.errorCode === "00000") {
@@ -288,8 +299,11 @@ export default {
       bucketName,
       visibility,
       bucketVisibilityModel,
+      name,
+      btName,
       checkFile,
       getBucketByName,
+      getValue,
       deleteBucket,
       addBucket,
       settingVisibility,
