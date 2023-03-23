8b6ea8cc0671463adc1464ccd36b14fd3f9f775e
RollW <73376341+Roll-W@users.noreply.github.com>
2023-03-23 11:11:59 +0800

    feat: add disk client application.

---
+++ cloudhub-disk-client/src/main/java/org/huel/cloudhub/client/disk/DiskClientApplication.java
@@ -0,0 +1,15 @@
+package org.huel.cloudhub.client.disk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author RollW
+ */
+@SpringBootApplication
+public class DiskClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiskClientApplication.class, args);
+    }
+}
