b370eb7c0388ed9abe359663ceaafcc9684ebe52
RollW <73376341+Roll-W@users.noreply.github.com>
2023-04-01 23:21:15 +0800

    fix(frontend): component refresh.

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,50 +1,46 @@
 <template>
-  <main>
-    <NavBar/>
-    <div class="d-flex flex-column p-4 flex-grow-1 allow-rolling">
-      <router-view/>
-      <FooterView/>
-    </div>
-
-  </main>
+    <main>
+        <NavBar/>
+        <div class="d-flex flex-column p-4 flex-grow-1 allow-rolling">
+            <router-view v-slot="{ Component }">
+                <component :is="Component" :key="$route.fullPath"/>
+            </router-view>
+            <FooterView/>
+        </div>
+    </main>
 </template>
 
-<script>
+<script setup>
 import NavBar from "@/components/common/NavBar";
 import FooterView from "@/components/common/Footer";
-export default {
-  name: "HomeView",
-  components:{
-    FooterView,
-    NavBar,
-  }
-}
+
 </script>
 
 <style scoped>
-body{
-  min-height: 100vh;
+body {
+    min-height: 100vh;
 }
 
-th,td{
-  text-align: center;
+th, td {
+    text-align: center;
 }
+
 .div-right-float {
-  float: right;
+    float: right;
 }
 
-main{
-  display: flex;
-  flex-wrap: nowrap;
-  width: auto;
-  height: 100vh;
-  max-height: 100vh;
-  overflow-x: auto;
-  overflow-y: hidden
+main {
+    display: flex;
+    flex-wrap: nowrap;
+    width: auto;
+    height: 100vh;
+    max-height: 100vh;
+    overflow-x: auto;
+    overflow-y: hidden
 }
 
-.allow-rolling{
-  overflow: auto;
+.allow-rolling {
+    overflow: auto;
 }
 </style>
 
