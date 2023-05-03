91f0e7535a5d4523ce5c0b5a5fb3b06f62c43486
RollW <73376341+Roll-W@users.noreply.github.com>
2023-05-03 20:45:43 +0800

    feat(disk-client): add query methods.

--- cloudhub-disk-client/src/main/java/org/huel/cloudhub/client/disk/database/dao/StorageMetadataDao.java
+++ cloudhub-disk-client/src/main/java/org/huel/cloudhub/client/disk/database/dao/StorageMetadataDao.java
@@ -1,12 +1,82 @@
 package org.huel.cloudhub.client.disk.database.dao;
 
+import org.huel.cloudhub.client.disk.domain.tag.dto.TagValue;
 import org.huel.cloudhub.client.disk.domain.userstorage.StorageMetadata;
 import space.lingu.light.Dao;
+import space.lingu.light.DaoConnectionGetter;
+import space.lingu.light.LightRuntimeException;
+import space.lingu.light.ManagedConnection;
+import space.lingu.light.Query;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author RollW
  */
 @Dao
-public interface StorageMetadataDao extends AutoPrimaryBaseDao<StorageMetadata> {
+public interface StorageMetadataDao
+        extends AutoPrimaryBaseDao<StorageMetadata>, DaoConnectionGetter {
 
+    @Query("SELECT * FROM storage_metadata WHERE id = {id}")
+    StorageMetadata getById(long id);
+
+    @Query("SELECT * FROM storage_metadata WHERE storage_id = {storageId}")
+    List<StorageMetadata> getByStorageId(long storageId);
+
+    @Query("SELECT * FROM storage_metadata WHERE storage_id = {storageId} AND tag_group_id = {tagGroupId}")
+    StorageMetadata getByStorageIdAndTagGroupId(long storageId, long tagGroupId);
+
+    @Query("SELECT * FROM storage_metadata WHERE tag_id = {tagId}")
+    List<StorageMetadata> getByTagId(long tagId);
+
+    default List<StorageMetadata> getByTagValues(List<TagValue> tagValues) {
+        if (tagValues.isEmpty()) {
+            return List.of();
+        }
+        StringBuilder sql = new StringBuilder("SELECT " +
+                "id, storage_id, name, value, tag_group_id, tag_id, deleted, create_time, update_time " +
+                "FROM storage_metadata WHERE ");
+        for (int i = 0; i < tagValues.size(); i++) {
+            sql.append("name = ")
+                    .append("?")
+                    .append(" AND value = ")
+                    .append("?");
+            if (i != tagValues.size() - 1) {
+                sql.append(" OR ");
+            }
+        }
+        ManagedConnection connection = getConnection();
+        PreparedStatement statement = connection.acquire(sql.toString());
+        int index = 1;
+        for (TagValue tagValue : tagValues) {
+            try {
+                statement.setString(index++, tagValue.name());
+                statement.setString(index++, tagValue.value());
+            } catch (Exception e) {
+                throw new LightRuntimeException(e);
+            }
+        }
+        List<StorageMetadata> storageMetadata = new ArrayList<>();
+        try (ResultSet resultSet = statement.executeQuery()) {
+            while (resultSet.next()) {
+                storageMetadata.add(new StorageMetadata(
+                        resultSet.getLong("id"),
+                        resultSet.getLong("storage_id"),
+                        resultSet.getString("name"),
+                        resultSet.getString("value"),
+                        resultSet.getLong("tag_group_id"),
+                        resultSet.getLong("tag_id"),
+                        resultSet.getBoolean("deleted"),
+                        0, 0
+                ));
+            }
+        } catch (Exception e) {
+            throw new LightRuntimeException(e);
+        }
+        connection.close();
+        return storageMetadata;
+    }
 }
--- cloudhub-disk-client/src/main/java/org/huel/cloudhub/client/disk/database/dao/UserFileStorageDao.java
+++ cloudhub-disk-client/src/main/java/org/huel/cloudhub/client/disk/database/dao/UserFileStorageDao.java
@@ -2,6 +2,7 @@ package org.huel.cloudhub.client.disk.database.dao;
 
 import org.huel.cloudhub.client.disk.domain.user.LegalUserType;
 import org.huel.cloudhub.client.disk.domain.userstorage.FileType;
+import org.huel.cloudhub.client.disk.domain.userstorage.StorageOwner;
 import org.huel.cloudhub.client.disk.domain.userstorage.UserFileStorage;
 import org.huel.cloudhub.web.data.page.Offset;
 import space.lingu.light.Dao;
@@ -41,4 +42,16 @@ public interface UserFileStorageDao extends AutoPrimaryBaseDao<UserFileStorage> 
 
     @Query("SELECT * FROM user_file_storage WHERE owner = {owner} AND owner_type = {ownerType} AND directory_id = {directoryId} AND name = {name}")
     UserFileStorage getById(long owner, LegalUserType ownerType, long directoryId, String name);
+
+    @Query("SELECT * FROM user_file_storage WHERE id IN {storageIds}")
+    List<UserFileStorage> getByIds(List<Long> storageIds);
+
+    @Query("SELECT * FROM user_file_storage WHERE id IN {storageIds} AND owner = {storageOwner.getOwnerId()} AND owner_type = {storageOwner.getOwnerType}")
+    List<UserFileStorage> getByIds(List<Long> storageIds, StorageOwner storageOwner);
+
+    @Query("SELECT * FROM user_file_storage WHERE id IN {storageIds} AND file_type = {fileType}")
+    List<UserFileStorage> getByIdsAndType(List<Long> storageIds, FileType fileType);
+
+    @Query("SELECT * FROM user_file_storage WHERE id IN {storageIds} AND file_type = {fileType} AND owner = {storageOwner.getOwnerId()} AND owner_type = {storageOwner.getOwnerType}")
+    List<UserFileStorage> getByIdsAndType(List<Long> storageIds, FileType fileType, StorageOwner storageOwner);
 }
