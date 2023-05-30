6d5e97796a02ab635c4be416bed11d39e2da8afd
RollW <73376341+Roll-W@users.noreply.github.com>
2023-05-30 21:48:17 +0800

    feat(disk-client): get prepared for user group service.

---
+++ cloudhub-disk-client/src/main/java/org/huel/cloudhub/client/disk/domain/usergroup/GroupSettingKeys.java
@@ -0,0 +1,12 @@
+package org.huel.cloudhub.client.disk.domain.usergroup;
+
+/**
+ * @author RollW
+ */
+public final class GroupSettingKeys {
+    public static final String GROUP_QUOTA = "group_quota";
+
+
+    private GroupSettingKeys() {
+    }
+}
