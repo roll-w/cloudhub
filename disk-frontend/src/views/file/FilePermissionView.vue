082a11243ffc1e6862c55e94eadb88b7b2e532f3
RollW <73376341+Roll-W@users.noreply.github.com>
2023-07-08 20:29:07 +0800

    feat(frontend): supports storage permission editing.

--- disk-frontend/src/views/file/FilePermissionView.vue
+++ disk-frontend/src/views/file/FilePermissionView.vue
@@ -7,6 +7,7 @@
             </n-h2>
             <div class="flex flex-fill justify-end">
                 <n-button secondary type="primary" @click="handleBack">返回</n-button>
+                <n-button secondary @click="handleBackToFolder">跳转到所在文件夹</n-button>
             </div>
         </div>
 
@@ -29,7 +30,9 @@
                     </n-radio-group>
                     <div class="pt-2 pb-4">
                         <n-alert :show-icon="false" type="default">
-                            {{ (permissions.find(permission => permission.value === publicPermissionSelect) || {}).info }}
+                            {{
+                                (permissions.find(permission => permission.value === publicPermissionSelect) || {}).info
+                            }}
                         </n-alert>
                     </div>
                 </div>
@@ -55,7 +58,7 @@
                         <tbody>
                         <tr v-for="permission in userPermissionsInput">
                             <td>{{ permission.userId }}</td>
-                            <td>{{ permission.permissions }}</td>
+                            <td>{{ formatPermission(permission.permissions) }}</td>
                             <td>
                                 <n-button-group>
                                     <n-button secondary type="primary">修改</n-button>
@@ -78,7 +81,7 @@
 
 
         <div class="py-4">
-            <n-button class="mt-5" type="primary" @click="message.success('保存成功')">保存</n-button>
+            <n-button class="mt-5" type="primary" @click="requestSetStoragePermission">保存</n-button>
         </div>
 
         <n-modal v-model:show="showUserPermissionModal"
@@ -87,21 +90,27 @@
                  title="用户权限"
                  transform-origin="center">
             <div>
-                <n-form-item label="用户名">
-                    <n-input v-model:value="usernameValue" placeholder="输入用户名" type="text"/>
+                <n-form-item label="用户名或ID">
+                    <n-input v-model:value="usernameValue" placeholder="输入用户名或用户ID" type="text"/>
                 </n-form-item>
                 <n-form-item label="权限">
                     <n-checkbox-group v-model:value="userPermissionCheckValue">
-                        <n-checkbox label="读取" value="read"/>
-                        <n-checkbox label="写入" value="write"/>
-                        <n-checkbox label="拒绝访问" value="denied"/>
+                        <n-checkbox label="读取" value="READ"/>
+                        <n-checkbox label="写入" value="WRITE"/>
+                        <n-checkbox label="拒绝访问" value="DENIED"/>
                     </n-checkbox-group>
                 </n-form-item>
                 <n-button-group>
                     <n-button type="primary" @click="handleAddUserPermission(usernameValue, userPermissionCheckValue)">
-                        添加
+                        保存
                     </n-button>
-                    <n-button secondary type="default" @click="showUserPermissionModal = false">取消</n-button>
+                    <n-button secondary type="default"
+                              @click="() => {
+                                  showUserPermissionModal = false
+                                  resetInput()
+                              }">
+                        取消
+                    </n-button>
                 </n-button-group>
             </div>
         </n-modal>
@@ -122,9 +131,10 @@ import {
     driveFilePageFolder,
     driveFilePageTypeAudio, driveFilePageTypeDocument,
     driveFilePageTypeImage,
-    driveFilePageTypeVideo
+    driveFilePageTypeVideo, driveFileSearchPage
 } from "@/router";
 import {getFileType} from "@/views/names";
+import {storagePublicPermission} from "@/request/api_urls";
 
 const router = useRouter();
 
@@ -180,14 +190,14 @@ const handleDeleteUserPermission = (id) => {
         positiveText: '确定',
         negativeText: '取消',
         onPositiveClick: () => {
-            userPermissionsInput.value = userPermissionsInput.value.filter(userPermission =>
-                userPermission.id !== id)
         }
     })
 }
 
 const handleAddUserPermission = (username, permissions = []) => {
     showUserPermissionModal.value = false
+    console.log(username, permissions)
+    requestAddUserPermission(username, permissions)
 }
 
 const resetInput = () => {
@@ -220,8 +230,15 @@ const handleBack = () => {
         })
         return;
     }
-
-
+    if (refer === 'search') {
+        router.push({
+            name: driveFileSearchPage,
+            query: {
+                keyword: source
+            }
+        }).catch()
+        return;
+    }
     if (error.value.message || fileInfo.value.parentId === 0) {
         router.push({
             name: driveFilePage
@@ -236,18 +253,77 @@ const handleBack = () => {
     })
 }
 
+const handleBackToFolder = () => {
+    if (error.value.message || fileInfo.value.parentId === 0) {
+        router.push({
+            name: driveFilePage
+        }).catch()
+        return
+    }
+    router.push({
+        name: driveFilePageFolder,
+        params: {
+            folder: fileInfo.value.parentId
+        }
+    }).catch()
+}
+
+const requestAddUserPermission = (username, permissions) => {
+    const config = createConfig(true)
+    proxy.$axios.put(api.storageUserPermission(ownerType, ownerId, storageType, id, username), {
+        value: permissions,
+    }, config).then(res => {
+        message.success('添加成功')
+        requestFileInfo()
+        resetInput()
+    }).catch(err => {
+        popUserErrorTemplate(notification, err,
+                '添加用户权限失败', '存储请求错误')
+        resetInput()
+    })
+
+}
+
+const formatPermission = (permissions) => {
+    const maped = permissions.map(permission => {
+        switch (permission) {
+            case 'READ':
+                return '读取'
+            case 'WRITE':
+                return '写入'
+            case 'DENIED':
+                return '拒绝访问'
+        }
+    })
+    return maped.join('、')
+}
+
+const requestSetStoragePermission = () => {
+    const config = createConfig(true)
+    proxy.$axios.put(api.storagePublicPermission(ownerType, ownerId, storageType, id), {
+        value: publicPermissionSelect.value,
+    }, config)
+            .then(res => {
+                requestFileInfo()
+                message.success('设置成功')
+            }).catch(err => {
+        popUserErrorTemplate(notification, err,
+                '设置存储权限失败', '存储请求错误')
+    })
+}
+
 const requestGetStoragePermissions = () => {
     const config = createConfig()
     proxy.$axios.get(api.getStoragePermissions(ownerType, ownerId, storageType, id), config)
-        .then(res => {
-            const permissions = res.data
-            storagePermission.value = permissions
+            .then(res => {
+                const permissions = res.data
+                storagePermission.value = permissions
 
-            publicPermissionSelect.value = permissions.publicPermission
-            userPermissionsInput.value = permissions.userPermissions
-        }).catch(err => {
+                publicPermissionSelect.value = permissions.publicPermission
+                userPermissionsInput.value = permissions.userPermissions
+            }).catch(err => {
         popUserErrorTemplate(notification, err,
-            '获取存储权限失败', '存储请求错误')
+                '获取存储权限失败', '存储请求错误')
     })
 }
 
@@ -258,18 +334,19 @@ const requestFileInfos = () => {
 const requestFileInfo = () => {
     const config = createConfig()
     proxy.$axios.get(api.getStorageInfo(ownerType, ownerId, storageType, id), config)
-        .then(res => {
-            fileInfo.value = res.data
-            requestFileInfos()
-        }).catch(err => {
-            error.value = {
-                status: err.status,
-                message: err.tip
-            }
-            popUserErrorTemplate(notification, err,
+            .then(res => {
+                fileInfo.value = res.data
+                requestFileInfos()
+            }).catch(err => {
+        error.value = {
+            status: err.status,
+            message: err.tip
+        }
+        popUserErrorTemplate(notification, err,
                 '获取文件信息失败', '存储请求错误')
     })
 }
+
 
 requestFileInfo()
 
