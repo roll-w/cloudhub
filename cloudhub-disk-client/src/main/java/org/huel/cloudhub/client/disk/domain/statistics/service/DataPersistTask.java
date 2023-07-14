69f17c68b2254cc1e340b9cef5c708521ab44813
RollW <73376341+Roll-W@users.noreply.github.com>
2023-07-14 17:46:37 +0800

    feat(disk-client): add data persist and archiving task.

---
+++ cloudhub-disk-client/src/main/java/org/huel/cloudhub/client/disk/domain/statistics/service/DataPersistTask.java
@@ -0,0 +1,107 @@
+package org.huel.cloudhub.client.disk.domain.statistics.service;
+
+import org.huel.cloudhub.client.disk.domain.statistics.Statistics;
+import org.huel.cloudhub.client.disk.domain.statistics.repository.StatisticsRepository;
+import org.huel.cloudhub.client.disk.jobs.JobEvent;
+import org.huel.cloudhub.client.disk.jobs.JobRegistry;
+import org.huel.cloudhub.client.disk.jobs.JobTask;
+import org.huel.cloudhub.client.disk.jobs.trigger.TimeJobTrigger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author RollW
+ */
+@Service
+public class DataPersistTask implements JobTask {
+    private static final Logger logger = LoggerFactory.getLogger(DataPersistTask.class);
+
+    private final StatisticsRepository statisticsRepository;
+    private final List<StatisticsPersistable> statisticsPersistables;
+
+    private final Map<String, Statistics> statisticsByKey =
+            new ConcurrentHashMap<>();
+    private final Map<String, Long> lastVersionByKey =
+            new ConcurrentHashMap<>();
+
+    public DataPersistTask(JobRegistry jobRegistry,
+                           StatisticsRepository statisticsRepository,
+                           List<StatisticsPersistable> statisticsPersistables) {
+        this.statisticsRepository = statisticsRepository;
+        this.statisticsPersistables = statisticsPersistables;
+        jobRegistry.register(
+                this,
+                TimeJobTrigger.of("0 0/5 * * * ?")
+        );
+        jobRegistry.register(
+                this,
+                TimeJobTrigger.of(System.currentTimeMillis() + 1000 * 10)
+        );
+    }
+
+
+    @Override
+    public void execute(JobEvent jobEvent) {
+        statisticsPersistables.forEach(this::tryPersist);
+    }
+
+    private void tryPersist(
+            StatisticsPersistable statisticsPersistable) {
+        String statisticsKey = statisticsPersistable.getStatisticsKey();
+        long version = statisticsPersistable.getStatisticsVersion();
+        if (lastVersionByKey.getOrDefault(statisticsKey, -1L)
+                == version) {
+            return;
+        }
+        Map<String, String> statisticsMap =
+                statisticsPersistable.getStatistics();
+        if (statisticsMap == null || statisticsMap.isEmpty()) {
+            logger.debug("StatisticsPersistable {}(key={}) return empty statistics, rescan.",
+                    statisticsPersistable.getClass().getName(), statisticsKey);
+            statisticsPersistable.rescanStatistics();
+            return;
+        }
+
+        lastVersionByKey.put(statisticsKey, version);
+        Statistics statistics = getByKey(statisticsKey);
+        if (StatHelper.equalsStatistics(statistics.getValue(),
+                statisticsMap)) {
+            return;
+        }
+        Statistics updated = statistics.toBuilder()
+                .setValue(statisticsMap)
+                .build();
+        statisticsByKey.put(statisticsKey, updated);
+        statisticsRepository.update(updated);
+
+        logger.debug("Persisted statistics: {}, version: {}",
+                statisticsKey, version);
+    }
+
+    private Statistics getByKey(String statisticsKey) {
+        Statistics statistics = statisticsByKey.get(statisticsKey);
+        if (statistics != null) {
+            return statistics;
+        }
+        Statistics queried =
+                statisticsRepository.getByKey(statisticsKey);
+        if (queried == null) {
+            queried = buildNewOne(statisticsKey);
+        }
+        statisticsByKey.put(statisticsKey, queried);
+        return queried;
+    }
+
+    private Statistics buildNewOne(String key) {
+        Statistics.Builder builder = Statistics.builder()
+                .setKey(key)
+                .setValue(new ConcurrentHashMap<>());
+        long id = statisticsRepository.insert(builder.build());
+        return builder.setId(id).build();
+    }
+}
